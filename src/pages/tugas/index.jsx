c5464da0c9a3a4cc98a5f37bd6073a6fd6ac883d
faairuzs <naufalfairuzs@gmail.com>
2023-10-04 13:29:31 +0700

    Update

--- src/pages/tugas/index.jsx
+++ src/pages/tugas/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  const [members, setMembers] = useState([
+  const [anggota, setanggota] = useState([
     "1. Fairuzsyah Naufal Fikri",
     "2. Renaldy Andara",
     "3. M Ghulam Abdul",
@@ -11,22 +11,22 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
-  const [filteredMembers, setFilteredMembers] = useState([]);
+  const [filteredanggota, setFilteredanggota] = useState([]);
 
   useEffect(() => {
-    const filtered = members.filter((member) =>
+    const filtered = anggota.filter((member) =>
       member.toLowerCase().includes(search.toLowerCase())
     );
 
-    setFilteredMembers(filtered);
-  }, [search, members]);
+    setFilteredanggota(filtered);
+  }, [search, anggota]);
 
   return (
     <div className="Main">
       <h2
         style={{
           borderRadius: "20px",
-          fontSize: "40px",
+          fontSize: "42px",
           color: "white",
         }}
       >
@@ -39,17 +39,17 @@ function App() {
           class="form-control"
           aria-label="Default"
           aria-describedby="inputGroup-sizing-default"
-          placeholder="Cari anggota..."
+          placeholder="Cari.."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
 
       <div>
-        {filteredMembers.length === 0 ? (
-          <div>Tidak ada anggota yang cocok</div>
+        {filteredanggota.length === 0 ? (
+          <div>Tidak ada yang cocok</div>
         ) : (
-          filteredMembers.map((member, index) => (
+          filteredanggota.map((member, index) => (
             <div key={index}>{member}</div>
           ))
         )}
